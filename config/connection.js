9fc27a6217a557d9b7e3f6402f1287ea73cc4c25
Charlie <caam_1275@hotmail.com>
2019-06-28 00:33:51 -0500

    confi json updated jwas db for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,23 +2,21 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user: "root",
-    password:"Carl_051",
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host:"localhost",
+        port:3306,
+        user: "root",
+        password:"Carl_051",
+        database: "burgers_db"
+    });
+};
 
 //Making connection ...
-3
-connection.connect(function(err){
-    if(err){
-        console.log("error connecting: " + err.stack);
-        return;
-    }
-    console.log("Yep, connected as id " + connection.threadId);
-});
 
 //Important to export always (if you neeed to use it in other file);
 
